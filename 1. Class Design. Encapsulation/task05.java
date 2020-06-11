2836a67a3c904c7f5f091ee422f855d385769dfa
SlobodianBohdan <62740608+SlobodianBohdan@users.noreply.github.com>
2020-06-11 20:44:32 +0300

    Update task05.java

--- 1. Class Design. Encapsulation/task05.java
+++ 1. Class Design. Encapsulation/task05.java
@@ -1,1 +1,37 @@
+// Create the Point class that correspond next class diagram:
+// Point class
+// A Point class, which models a 2D point with x and y coordinates should contains:
+// Two private instance variables x and y  of int type.
+// A constructor that constructs a point with the given x and y coordinates.
+// A method getXYPair() which returns the x and y in a 2-element int array.
+// A method called distance(int x, int y) that returns the distance from this point to another point at the given (x, y) coordinates.
+// An overloaded distance(Point point) method that returns the distance from this point to the given Point instance.
+// Another overloaded distance() method that returns the distance from this point to the origin (0, 0)
+// * For correct passing of all tests don't use print and println methods in your code.
 
+
+class Point{
+    private int x;
+    private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int[] getXYPair() {
+        return new int[]{x, y};
+    }
+
+    public double distance(int x, int y) {
+        return Math.sqrt(Math.pow(this.x - x, 2) + Math.pow(this.y - y, 2));
+    }
+
+    public double distance(Point point) {
+        return Math.sqrt(Math.pow(point.x - x, 2) + Math.pow(point.y - y, 2));
+    }
+
+    public double distance(){
+        return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+    }
+}
