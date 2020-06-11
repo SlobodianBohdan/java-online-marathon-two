0fe2a4e14353f78a62edf8dca9ddf26091513791
SlobodianBohdan <62740608+SlobodianBohdan@users.noreply.github.com>
2020-06-11 14:19:13 +0300

    Update task01.java

--- 1. Class Design. Encapsulation/task01.java
+++ 1. Class Design. Encapsulation/task01.java
@@ -1,1 +1,8 @@
+// Create a Pet class with public fields name of type String and age of type int that are not initialized.
+// * For correct passing of all tests don't use print and println methods in your code.
 
+
+class Pet{
+    public String name;
+    public int age;
+}
