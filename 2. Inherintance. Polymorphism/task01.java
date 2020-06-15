5e1a08863a9869992d2d2fbc0c87d124e99a34b9
SlobodianBohdan <62740608+SlobodianBohdan@users.noreply.github.com>
2020-06-15 20:51:53 +0300

    Update task01.java

--- 2. Inherintance. Polymorphism/task01.java
+++ 2. Inherintance. Polymorphism/task01.java
@@ -1,1 +1,59 @@
+// Please, make refactoring of the code:
+// class Person {
+//     String childIDNumber;    
+// }
 
+// class Child {
+//     int age;
+//     String healthInfo;
+//     String name;
+//     String getHealthStatus(){ return name +" " + healthInfo; }
+// }
+
+// class Adult {
+//     int age;
+//     String healthInfo;
+//     String passportNumber;   
+//     String name;
+//     String getHealthStatus(){ return name +" " + healthInfo; }
+// }
+// We know that adult  doesn't have childIDNumber.
+// Child doesn't have passportNumber.
+// Create a public constructor in each class to initialize all their fields (make the first parameter of type int).
+
+class Person{
+    String name;
+    int age;
+    String healthInfo;
+
+    public Person(int age, String name, String healthInfo) {
+        this.name = name;
+        this.age = age;
+        this.healthInfo = healthInfo;
+    }
+    
+    public String getHealthStatus(){
+        return healthInfo;
+    }
+    
+}
+
+class Child extends Person{
+    private String childIDNumber;
+
+    public Child(int age, String name, String healthInfo, String childIDNumber) {
+        super(age, name, healthInfo);
+        this.childIDNumber = childIDNumber;
+    }
+}
+
+
+
+class Adult extends Person{
+    private String passportNumber;
+
+    public Adult(int age, String name, String healthInfo, String passportNumber) {
+        super(age, name, healthInfo);
+        this.passportNumber = passportNumber;
+    }
+}
