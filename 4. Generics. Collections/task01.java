30240a5eaf4a1756d42cd28d3bd2b0814e699c1f
SlobodianBohdan <62740608+SlobodianBohdan@users.noreply.github.com>
2020-06-22 11:26:17 +0300

    Update task01.java

--- 4. Generics. Collections/task01.java
+++ 4. Generics. Collections/task01.java
@@ -1,1 +1,24 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
+class MyUtils {
+    public Map<String, List<String>> createNotebook(Map<String, String> phones) {
+        Map<String, List<String>> map = new HashMap<>();
+        if (phones != null) {
+            Set<String> set = new HashSet<>(phones.values());
+            set.forEach(i -> {
+                List<String> list = phones.entrySet()
+                        .stream()
+                        .filter(e ->(Objects.nonNull(e.getValue()) && e.getValue().equals(i)) || e.getValue() == i)
+                        .map(Map.Entry::getKey)
+                        .collect(Collectors.toList());
+                map.put(i, list);
+            });
+        }
+        return map;
+    }
+}
