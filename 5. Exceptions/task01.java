824b618e2ea39f1e4a9ec2ad02a81465013f7ef3
SlobodianBohdan <62740608+SlobodianBohdan@users.noreply.github.com>
2020-06-25 10:45:34 +0300

    Update task01.java

--- 5. Exceptions/task01.java
+++ 5. Exceptions/task01.java
@@ -1,7 +1,7 @@
-Create a method for calculating an area of a rectangle
-int squareRectangle(int a, int b), which should throw an IllegalArgumentException if at least one of its arguments is non positive. 
-Create trySquareRectangle method which takes two parameters and calls squareRectangle to evaluate an area of a rectangle. Catch exceptions that can be generated.
-trySquareRectangle shouldn't generate any exceptions. In case when one or two parameters are negative the method should return -1;
+// Create a method for calculating an area of a rectangle
+// int squareRectangle(int a, int b), which should throw an IllegalArgumentException if at least one of its arguments is non positive. 
+// Create trySquareRectangle method which takes two parameters and calls squareRectangle to evaluate an area of a rectangle. Catch exceptions that can be generated.
+// trySquareRectangle shouldn't generate any exceptions. In case when one or two parameters are negative the method should return -1;
 
 class Operation{
     public static  int squareRectangle (int a, int b) {
