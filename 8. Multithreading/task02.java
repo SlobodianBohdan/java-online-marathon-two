93b44577b031f6d131052d4b1f83ee44a14480b1
SlobodianBohdan <62740608+SlobodianBohdan@users.noreply.github.com>
2020-07-07 19:46:22 +0300

    Update task02.java

--- 8. Multithreading/task02.java
+++ 8. Multithreading/task02.java
@@ -1,1 +1,20 @@
 
+import java.util.function.BinaryOperator;
+
+class Accountant {
+    public static int sum(int x, int y) {
+        BinaryOperator<Integer> binaryOperator =
+                (integer, integer2) -> integer + integer2;
+        ParallelCalculator parallelCalculator = new ParallelCalculator(binaryOperator, x, y);
+        Thread thread = new Thread(parallelCalculator);
+        thread.start();
+        synchronized (thread){
+            try {
+                thread.wait();
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+        return parallelCalculator.result;
+    }
+}
