c2f9512c5e8986cdc36b77167c5adb2902d9f0ec
SlobodianBohdan <62740608+SlobodianBohdan@users.noreply.github.com>
2020-07-07 19:46:49 +0300

    Update task01.java

--- 8. Multithreading/task01.java
+++ 8. Multithreading/task01.java
@@ -1,1 +1,20 @@
+import java.util.function.BinaryOperator;
 
+
+class ParallelCalculator implements Runnable{
+    BinaryOperator<Integer> binaryOperator;
+    int operand1;
+    int operand2;
+    int result;
+
+    public ParallelCalculator(BinaryOperator<Integer> binaryOperator, int operand1, int operand2) {
+        this.binaryOperator = binaryOperator;
+        this.operand1 = operand1;
+        this.operand2 = operand2;
+    }
+
+    @Override
+    public void run() {
+        result = binaryOperator.apply(operand1, operand2);
+    }
+}
