f923a186a0c24bce6755e1ad09133be1a7ab552a
SlobodianBohdan <62740608+SlobodianBohdan@users.noreply.github.com>
2020-07-09 22:45:08 +0300

    Update task02.java

--- 9. Stream API. Data and Time/task02.java
+++ 9. Stream API. Data and Time/task02.java
@@ -1,1 +1,7 @@
+// Write a method to get the date n-years m-months and k-days after today using new DateTime API.
+// Return the obtained date in the format ISO_LOCAL_DATE.
+// * For correct passing of all tests don't use print and println methods in your code.
 
+public static String getDateAfterToday(int years, int months, int days) {
+    return LocalDate.now().plus(Period.of(years, months, days)).toString();
+}
