f6f7e80dac17a87c806d53db50edda50a5d89605
SlobodianBohdan <62740608+SlobodianBohdan@users.noreply.github.com>
2020-07-09 21:58:31 +0300

    Update task01.java

--- 9. Stream API. Data and Time/task01.java
+++ 9. Stream API. Data and Time/task01.java
@@ -1,1 +1,8 @@
+// Write a method to check if a year is a leap year or not, using for this the LocalDate class.
+// If a year is leap then method should return true, otherwise - false.
+// * For correct passing of all tests don't use print and println methods in your code.
 
+
+public static boolean isLeapYear(int year) {
+    return LocalDate.ofYearDay(year, 1).isLeapYear();
+}
