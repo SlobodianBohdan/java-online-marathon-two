37b99a5f06b59067f3b0adfd2bd87f91ab2cce72
SlobodianBohdan <slobodianbohdan1997@gmail.com>
2020-11-25 21:24:38 +0200

    Add Spring Security!

---
+++ 17. SpringSecurity/src/main/java/com/softserve/sprint16/security/WebSecurityInitializer.java
@@ -0,0 +1,8 @@
+package com.softserve.sprint16.security;
+
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class WebSecurityInitializer
+        extends AbstractSecurityWebApplicationInitializer {
+}
